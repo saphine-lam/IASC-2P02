7ad1437f5f2a0928a713454fba6d690e7bd42155
saphine-lam <>
2026-02-01 12:28:49 -0500

    Add Prototype 2

--- prototype2/script.js
+++ prototype2/script.js
@@ -35,7 +35,7 @@ const camera = new THREE.PerspectiveCamera(
     100
 )
 scene.add(camera)
-camera.position.set(0, 0, 5)
+camera.position.set(-2, 3, -5)
 
  //Renderer
  const renderer = new THREE.WebGLRenderer({
@@ -58,11 +58,53 @@ const testSphere = new THREE.Mesh(sphereGeometry, sphereMaterial)
 
 scene.add(testSphere)
 
+// Plane
+const planeGeometry = new THREE.PlaneGeometry( 10, 10, 50, 50)
+const planeMaterial = new THREE.MeshBasicMaterial({
+    color: new THREE.Color('white'),
+    side: THREE.DoubleSide,
+    wireframe: true
+})
+const plane = new THREE.Mesh(planeGeometry, planeMaterial)
+plane.rotation.x = Math.PI * 0.5
+
+scene.add(plane)
+
 /********
  ** UI **
  *******/
 // UI
 const ui = new dat.GUI()
+
+//UI Object
+const uiObject = {
+    speed: 1,
+    distance: 1
+}
+
+//plane UI 
+const planeFolder = ui.addFolder('Plane')
+
+planeFolder
+    .add(planeMaterial, 'wireframe')
+    .name("Toggle Wireframe")
+
+//testSphere UI
+const sphereFolder = ui.addFolder('Sphere')
+
+sphereFolder
+    .add(uiObject, 'speed')
+    .min(0.1)
+    .max(10)
+    .step(0.1)
+    .name("Speed")
+
+sphereFolder
+    .add(uiObject, 'distance')
+    .min(0.1)
+    .max(10)
+    .step(0.1)
+    .name("Distance")
 
  /******************* 
  ** ANIMATION LOOP **
@@ -74,6 +116,9 @@ const animation = () =>
     //Return elapsedTime
     const elapsedTime = clock.getElapsedTime()
     console.log(elapsedTime)
+
+    //Animate Sphere
+    testSphere.position.y = Math.sin(elapsedTime * uiObject.speed) * uiObject.distance
 
     //Update OrbitControls
     controls.update()
